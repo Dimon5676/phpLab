3fb41d20c089e9f75ee1e26ef38910390ea592b0
Dimon5676 <Dimon5676@yandex.ru>
2020-12-17 21:12:49 +0300

    Fixed repo

---
+++ phpLab/resources/views/add.blade.php
@@ -0,0 +1,55 @@
+@extends("layouts.app")
+
+@section("content")
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Add note') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('add') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="title" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus>
+
+                                    @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Text') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="text" type="text" class="form-control @error('text') is-invalid @enderror" name="text" required autocomplete="text">
+
+                                    @error('text')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Submit') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ phpLab/resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.app")
+
+@section('content')
+    <h1 align="center">Welcome to my site</h1>
+@endsection
